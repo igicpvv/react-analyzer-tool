abb7ba41b6ca3c70d4be2bef78f76d782f29a283
igicpvv <plinio@igic.com.br>
2025-02-20 17:01:41 -0300

    clean

--- src/bin/refactor_style_inline.js
+++ src/bin/refactor_style_inline.js
@@ -98,9 +98,6 @@ for (const fileIndex in files) {
                 .filter(x => x.name.name == "style");
             if (list.length == 0) return;
 
-            if (file.name.indexOf("ClientSelection") != -1)
-                console.log(file.name);
-
             for (const prop of list)
                 if (prop.value.expression.type == "ObjectExpression")
                     if (!prop.value.expression.properties.some(x => !(x.value.type == "StringLiteral" || x.value.type == "NumericLiteral")))
